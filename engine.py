37de785c37d7ef85cd2ead06f8d488e404e1dea7
Farel-Putra-Albana <2101020057@student.umrah.ac.id>
2024-12-13 20:29:56 +0700

    Update Fix code engine.py

--- engine.py
+++ engine.py
@@ -103,10 +103,11 @@ def evaluate(model, data_loader, device, labels_dict):
         model_time = time.time() - model_time
 
         res = {target["image_id"].item(): output for target, output in zip(targets, outputs)}
-        evaluator_time = time.time()
-        coco_evaluator.update(res)
-        evaluator_time = time.time() - evaluator_time
-        metric_logger.update(model_time=model_time, evaluator_time=evaluator_time)
+        if res:  
+            evaluator_time = time.time()
+            coco_evaluator.update(res)
+            evaluator_time = time.time() - evaluator_time
+            metric_logger.update(model_time=model_time, evaluator_time=evaluator_time)
 
         # Mengumpulkan label ground truth dan prediksi
         for target, output in zip(targets, outputs):
