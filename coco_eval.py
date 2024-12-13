f129377bba8d827d2ebd07b3d397a23171000940
Farel-Putra-Albana <2101020057@student.umrah.ac.id>
2024-12-13 19:58:26 +0700

    Update Fix code 3 coco_eval.py

--- coco_eval.py
+++ coco_eval.py
@@ -188,7 +188,7 @@ def merge(img_ids, eval_imgs):
         merged_eval_imgs.append(p)
 
     merged_img_ids = np.array(merged_img_ids)
-    merged_eval_imgs = np.concatenate(merged_eval_imgs, 2)
+    merged_eval_imgs = np.concatenate(merged_eval_imgs, 2)  
 
     # keep only unique (and in sorted order) images
     merged_img_ids, idx = np.unique(merged_img_ids, return_index=True)
